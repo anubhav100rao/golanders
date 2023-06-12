c25de99e900cba599454c1905a09797afcf8076a
anubhav100rao <anubhav100rao@gmail.com>
2023-06-13 01:37:50 +0530

    updated

---
+++ conditionals/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var number int = 100 // Your number goes here.
+
+	switch { // Equivalent to `switch true`
+	case number%2 == 0:
+		fmt.Println("The number", number, "is even")
+	case number%2 != 0:
+		fmt.Println("The number", number, "is odd")
+	}
+
+	letter := "a"
+
+	switch letter {
+	case "a", "e", "i", "o", "u":
+		fmt.Println("The letter", letter, "is a vowel")
+	default:
+		fmt.Println("The letter", letter, "is a consonant")
+	}
+
+	fmt.Scanf("%d", &number)
+
+	// Write your code here.
+	switch {
+	case number > 0:
+		fmt.Println("Positive!")
+	case number < 0:
+		fmt.Println("Negative!")
+	default:
+		fmt.Println("Zero!")
+	}
+}
