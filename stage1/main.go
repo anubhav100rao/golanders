293f1017292693449cd8bbb9c6e78c4dbbe66965
anubhav100rao <anubhav100rao@gmail.com>
2023-06-30 01:48:38 +0530

    updated

---
+++ stage1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Print("Directory is not specified")
+		return
+	}
+	root := os.Args[1]
+	fmt.Println(root)
+
+	err := filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+		if !info.IsDir() {
+			fmt.Printf("%q\n", path)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal("walk is not completed")
+		return
+	}
+}
