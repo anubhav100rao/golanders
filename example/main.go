7098f7aeb2aa2cef25269927b92d587175a3af48
anubhav100rao <anubhav100rao@gmail.com>
2023-07-01 23:28:06 +0530

    updated

--- example/main.go
+++ example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"path/filepath"
 )
 
@@ -25,5 +26,26 @@ func main() {
 			log.Fatal(err)
 		}
 		fmt.Println(abs)
+	}
+
+	fmt.Println(filepath.IsAbs("/home/User/GolandProjects/example")) // true
+	fmt.Println(filepath.IsAbs("/.bashrc"))                          // true
+	fmt.Println(filepath.IsAbs(".bashrc"))                           // false
+	fmt.Println(filepath.IsAbs("/"))                                 // true Linux/macOS | false Windows
+	fmt.Println(filepath.IsAbs(""))                                  // false
+
+	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+		if info.IsDir() {
+			fmt.Println("Directory:", path, "size:", info.Size(), "bytes")
+		} else {
+			fmt.Println("File:", path, "size:", info.Size(), "bytes")
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
 }
